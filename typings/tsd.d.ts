c6dad8d256c2ab0955f4ca278195b682ca8e655e
Michael Van Sickle <mvansickle1@gmail.com>
2016-04-25 03:02:06 -0400

    Allow CI to be run on Browserstack or Sauce Labs
    Refs dojo/dojo2-package-template#22

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,1 +1,2 @@
-///<reference path="../node_modules/dojo-core/_typings/dojo-core/dojo-core-2.0.0-pre.d.ts"/>
+///<reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core-2.0.0-pre.d.ts"/>
+///<reference path="../node_modules/dojo-core/typings/symbol-shim/symbol-shim.d.ts"/>
